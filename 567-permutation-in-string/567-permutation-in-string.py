9f579f743265dcc8c4c85df70ad3844707211eff
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-05-11 10:40:40 -0400

    Time: 69 ms (90.89%), Space: 13.9 MB (68.64%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,28 +1,29 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        p_set = {}
-        for chr in s1:
-            if chr not in p_set:
-                p_set[chr] = 0
-            p_set[chr] += 1
+        dict = {}
+        for letter in s1:
+            if letter not in dict:
+                dict[letter] = 0
+            dict[letter] += 1
 
 
-        l, matches = 0, 0
-        for r in range(len(s2)):
-            if s2[r] in p_set:
-                p_set[s2[r]] -= 1
-                if p_set[s2[r]] == 0:
+        matches, left = 0, 0
+        for right in range(len(s2)):
+            if s2[right] in dict:
+                dict[s2[right]] -= 1
+                if dict[s2[right]] == 0:
                     matches += 1
 
-            if matches == len(p_set):
+            if matches == len(dict):
                 return True
 
-            if r >= len(s1)-1:
-                if s2[l] in p_set:
-                    if p_set[s2[l]] == 0:
-                        matches -= 1 
-                    p_set[s2[l]] += 1
-                l += 1
+            if right >= len(s1) - 1:
+                if s2[left] in dict:
+                    if dict[s2[left]] == 0:
+                        matches -= 1
+                    dict[s2[left]] += 1
+
+                left += 1
 
         return False
         
