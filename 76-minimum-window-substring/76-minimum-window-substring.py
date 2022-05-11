3e9f530085170353ba31db8e672d93857a806b12
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-05-11 11:59:03 -0400

    Time: 174 ms (41.89%), Space: 14.8 MB (37.77%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,50 +1,38 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # return empty string if |pattern| > |str|
-        if len(t) > len(s):
-            return ""
-        
-        # loading all chars into a dict
-        dict = {}
-        for c in t:
-            if c not in dict:
-              dict[c] = 0
-            dict[c] += 1
+        window_start, matched, substr_start = 0, 0, 0
+        min_length = len(s) + 1
+        char_frequency = {}
 
+        for chr in t:
+            if chr not in char_frequency:
+              char_frequency[chr] = 0
+            char_frequency[chr] += 1
 
-        window_start, curr_word, min_word, matches, found = 0, "", s, 0, False
-        #s.replace('a', '')
-
-        # iterating through chars of string
+        # try to extend the range [window_start, window_end]
         for window_end in range(len(s)):
-            right = s[window_end]
-            curr_word += right
-            if right in dict:
-              dict[right] -= 1
-              if dict[right] == 0:
-                matches += 1
+            right_char = s[window_end]
+            if right_char in char_frequency:
+              char_frequency[right_char] -= 1
+              if char_frequency[right_char] >= 0:  # Count every matching of a character
+                matched += 1
 
-            while matches == len(dict):
-              # all chars in pattern have been found at least once
-              found = True
+            # Shrink the window if we can, finish as soon as we remove a matched character
+            while matched == len(t):
+              if min_length > window_end - window_start + 1:
+                min_length = window_end - window_start + 1
+                substr_start = window_start
 
-              # update smallest substring
-              if len(curr_word) < len(min_word):
-                min_word = curr_word
-
-              # decrement char from dict
-              left = s[window_start]
-              if left in dict:
-                if dict[left] == 0:
-                  matches -= 1
-                dict[left] += 1
-
-              # slide window forward
+              left_char = s[window_start]
               window_start += 1
-              curr_word = curr_word[1:]
+              if left_char in char_frequency:
+                # Note that we could have redundant matching characters, therefore we'll decrement the
+                # matched count only when a useful occurrence of a matched character is going out of the window
+                if char_frequency[left_char] == 0:
+                  matched -= 1
+                char_frequency[left_char] += 1
 
-
-        if not found:
+        if min_length > len(s):
             return ""
-        return min_word
+        return s[substr_start:substr_start + min_length]
         
