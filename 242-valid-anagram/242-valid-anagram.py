bb59553448f1812911efe96c10529b3bf586a626
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-05-18 10:37:57 -0400

    Time: 56 ms (73.40%), Space: 14.5 MB (67.44%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,25 +1,60 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+        
+#         set = {}
+#         for c in s: 
+#             if c not in set:
+#                 set[c] = 0
+#             set[c] += 1
+        
+#         matches = 0
+        
+#         for r in range(len(t)):
+#             if t[r] in set:
+#                 set[t[r]] -= 1
+#                 if set[t[r]] == 0:
+#                     matches += 1
+                    
+#             if matches == len(set):
+#                 return True
+            
+#         return False
+            
+        
+        # if they are not the same length
         if len(s) != len(t):
             return False
         
-        set = {}
-        for c in s: 
-            if c not in set:
-                set[c] = 0
-            set[c] += 1
+        # if they are the same length
+        # count the number of each letter in s using a dictionary
+        letters = {}
+        for letter in s:
+            if letter not in letters:
+                letters[letter] = 0
+            letters[letter] += 1
         
-        matches = 0
         
-        for r in range(len(t)):
-            if t[r] in set:
-                set[t[r]] -= 1
-                if set[t[r]] == 0:
-                    matches += 1
-                    
-            if matches == len(set):
-                return True
+        # go through t and make sure it matches the number of letters in s
+        for i in range(len(t)):
+            if t[i] not in letters:
+                return False
+            else:
+                letters[t[i]] -= 1
+        
+        for count in letters.values():
+            if count != 0:
+                return False
+        
+        return True
+    
+    #time complexity: 
+        # putting every letter of s in dictionary:      O(n)
+        # going through t:                              O(n)
+        # making sure the values are all 0:             O(n)
+        #                                               = O(3n) = O(n)
             
-        return False
-            
-        
+    #space complexity:
+        # putting every letter of s in dictionary:      O(n)
+                                
