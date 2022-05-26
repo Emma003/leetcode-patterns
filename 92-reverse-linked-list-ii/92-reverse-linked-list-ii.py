0defb37fbe17c89bd50673076b25de8ca6f935f0
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-05-26 12:33:08 -0400

    Time: 44 ms (47.12%), Space: 14.1 MB (51.40%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -5,36 +5,33 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], p: int, q: int) -> Optional[ListNode]:
-        # three steps
-            # place two pointers at p and p-1
-            # reverse the sublist
-            # connect the head and tail of the new sublist to the left and right                    #ends
-            
-        dummy = ListNode(-1,head)
-        lprv, curr = dummy, head
-        
-        # place two pointers at p and p-1
-        i = 0
-        while i < (p-1) and curr != None:
+        dummy = ListNode(-1, head)
+        front_end = dummy
+        curr = head
+
+        # get to the start of the sublist
+        i = 1
+        while i<p:
             curr = curr.next
-            lprv = lprv.next
+            front_end = front_end.next
             i+=1
-        
-        #reverse sublist
-        prev = nxt = None
+
+        # reverse the sublist
         j = p
-        while curr and j < (q+1): # after this loop curr = next = q+1 and prv=q
+        prv, nxt = None, None
+        while curr and j<=q:
             nxt = curr.next
-            curr.next = prev
-            prev = curr
+            curr.next = prv
+            prv = curr
             curr = nxt
             j+=1
-        
-        #connect
-        lprv.next.next = curr
-        lprv.next = prev
-        
+
+        # connect the start and end of the sublist
+        front_end.next.next = nxt
+        front_end.next = prv
+
         return dummy.next
+
         
             
 
