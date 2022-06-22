5e95a7f39f83394b3f42a8f90143a8c6375218e7
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-06-22 12:27:23 -0400

    Time: 1011 ms (84.66%), Space: 20.5 MB (16.56%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,21 +1,30 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
+        
+        
         baskets = {}
-        start, max_fruits = 0, -1
-
+        start, max_fruits = 0, 0
+        
         for end in range(len(fruits)):
-            r = fruits[end]
-            if r not in baskets:
-                baskets[r] = 0
-            baskets[r] += 1
-
+            right = fruits[end]
+            
+            if right not in baskets:
+                baskets[right] = 0
+            baskets[right] += 1
+            
             while len(baskets) > 2:
-                l = fruits[start]
-                baskets[l] -= 1
-                if baskets[l] == 0:
-                    del baskets[l]
-                start +=1
-
-            max_fruits = max(max_fruits, end-start+1)
+                left = fruits[start]
+                
+                baskets[left] -= 1
+                if baskets[left] == 0: 
+                    del baskets[left]
+                    
+                start += 1
+                    
+        
+            max_fruits = max(max_fruits, end - start + 1)
+            
         return max_fruits
-
+            
+            
+        
