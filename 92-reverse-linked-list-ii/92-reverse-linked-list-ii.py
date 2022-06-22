52ccfadc091a684699be42ae779562e79e5b959d
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-06-22 12:05:08 -0400

    Time: 53 ms (31.62%), Space: 14 MB (51.21%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.py
@@ -4,35 +4,45 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], p: int, q: int) -> Optional[ListNode]:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        
+        #if len(list) < 2
+        if not head or not head.next: 
+            return head
+        
+        
+        # two pointers: one at the start of the list, the other one node before the start
         dummy = ListNode(-1, head)
-        front_end = dummy
-        curr = head
-
-        # get to the start of the sublist
-        i = 1
-        while i<p:
-            curr = curr.next
-            front_end = front_end.next
-            i+=1
-
-        # reverse the sublist
-        j = p
-        prv, nxt = None, None
-        while curr and j<=q:
+        start = head
+        left_bridge = dummy
+        
+        # positioning the start pointer (tail of the reverse linked list) right before the start of the sublist
+        i = 0
+        while start and i < left-1:
+            start = start.next
+            left_bridge = left_bridge.next
+            i += 1
+            
+        
+        
+        # reversing sublist
+        curr = start
+        prv = nxt = None
+        while curr and left <= right:
             nxt = curr.next
             curr.next = prv
             prv = curr
             curr = nxt
-            j+=1
-
-        # connect the start and end of the sublist
-        front_end.next.next = nxt
-        front_end.next = prv
-
+            
+            left += 1
+                
+        # reattaching the ends
+        left_bridge.next.next = curr
+        left_bridge.next = prv
+        
         return dummy.next
+    
 
         
-            
-
+        
         
