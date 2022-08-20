772c38cf94267274973ab02ba3883917073d6681
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-08-20 09:45:09 -0400

    Time: 53 ms (87.49%), Space: 14.4 MB (45.09%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,21 +6,11 @@ class Solution(object):
         :rtype: List[int]
         """
         set = {}
-        for i, n in enumerate(nums):
-            if (target - n) in set:
-                return [set[target - n], i]
+        res = []
+        
+        for i,n in enumerate(nums):
+            targSum = target - n
+            if targSum in set:
+                return [set[targSum], i]
+            
             set[n] = i
-        
-        
-        
-        
-        
-        
-        
-        
-        map = {}
-        for i in range(len(nums)):
-            y = target - nums[i]
-            if y in map:
-                return [map[y], i]
-            map[nums[i]] = i
