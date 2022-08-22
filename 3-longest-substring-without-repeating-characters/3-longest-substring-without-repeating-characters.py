4bca2895c9b8fc3be6c80987ccd33be23c444a27
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-08-22 16:12:27 -0400

    Time: 89 ms (69.48%), Space: 14 MB (93.64%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,15 +1,24 @@
+# dict = 
+# [,]
+# [b,1]
+# [c,1]
+
+# maxW = 3
+
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:     
-        set = {}
-        window_start, max_size = 0, 0
-
-        for window_end in range(len(s)):
-            right_char = s[window_end]
-
-            if right_char in set:
-                window_start = max(window_start, set[right_char] + 1)
-
-            set[right_char] = window_end
-            max_size = max(max_size, window_end - window_start + 1)
-
-        return max_size
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        maxWindow = 0
+        dict = {}
+        
+        for right in range(len(s)):
+            
+            while s[right] in dict and left<=right:
+                del dict[s[left]]
+                left += 1
+                
+            dict[s[right]] = 1
+            
+            maxWindow = max(maxWindow, right - left + 1)
+            
+        return maxWindow
