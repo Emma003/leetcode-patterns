529dc8dc0e3797631f9b2c88764a7fd5d75b9eab
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-08-24 11:51:33 -0400

    Time: 106 ms (41.31%), Space: 18.1 MB (41.29%) - LeetHub

--- 155-min-stack/155-min-stack.py
+++ 155-min-stack/155-min-stack.py
@@ -1,29 +1,32 @@
 class MinStack:
 
     def __init__(self):
-        self.q = deque()
-        self.mindeq = deque()
+        self.stack = []
+        
+        #this array will mirror the stack and keep track of what was the smallest value when x was added to the stack
+        self.minSoFar = []
 
     def push(self, val: int) -> None:
-        self.q.append(val)
+        self.stack.append(val)
         
-        if self.mindeq:
-            val = min(val, self.mindeq[-1])
-            
-        self.mindeq.append(val)
-            
+        #if minStack is empty, then current value is the smallest
+        if not self.minSoFar:
+            self.minSoFar.append(val)
+        #append the min val between curr val and curr min
+        else:
+            currMin = self.minSoFar[-1]
+            self.minSoFar.append(min(currMin, val))
+        
 
     def pop(self) -> None:
-        self.mindeq.pop()
-        self.q.pop()
+        self.stack.pop()
+        self.minSoFar.pop()
 
     def top(self) -> int:
-        return self.q[-1]
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.mindeq[-1]
-        
-        
+        return self.minSoFar[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
