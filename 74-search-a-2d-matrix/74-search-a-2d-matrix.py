ac630d95ccb67cbe0679223cc181ddf58a87a572
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-08-25 11:52:16 -0400

    Time: 77 ms (36.65%), Space: 14.4 MB (89.28%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -2,66 +2,36 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         ROWS, COLS = len(matrix), len(matrix[0])
         
-        #binary search on rows (O(logM))
-        top, bot = 0, ROWS-1
-        while (top <= bot):
-            row = (top + bot) // 2
+        #first binary search
+        top, bottom = 0, ROWS-1
+        
+        while top <= bottom:
+            mid = (top+bottom) // 2
             
-            if target < matrix[row][0]:
-                bot = row - 1
-            elif target > matrix[row][-1]:
-                top = row + 1
+            if target < matrix[mid][0]:
+                bottom = mid -1
+            elif target > matrix[mid][-1]:
+                top = mid + 1
             else:
                 break
-        
-        #checking if the number wasnt found at all (failed while loop condition):
-        if (top > bot):
+                
+        #check that the item is within bounds:
+        if top > bottom:
             return False
         
-        #doing binary search on the row found(O(log(N)))
-        # row = (top + bot) // 2
-        l, r = 0, COLS-1
-        arrOfInterest = matrix[row]
+        #second binary search
+        left, right = 0, COLS-1
+        arr = matrix[mid]
         
-        while(l <= r):
-            m = (l+r) // 2
-            if target > arrOfInterest[m]:
-                l = m+1
-            elif target < arrOfInterest[m]:
-                r = m-1
+        while left <= right:
+            m = (left+right) // 2
+            
+            if target < arr[m]:
+                right = m-1
+            elif target > arr[m]:
+                left = m+1
             else:
                 return True
-        
+            
         return False
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        
-        
+                
