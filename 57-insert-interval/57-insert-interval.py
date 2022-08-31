a12d6fef91a53bda21ccc08305726129d7ccd3d6
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-08-31 12:39:13 -0400

    Time: 153 ms (23.72%), Space: 17.3 MB (92.16%) - LeetHub

--- 57-insert-interval/57-insert-interval.py
+++ 57-insert-interval/57-insert-interval.py
@@ -1,23 +1,24 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        l, r = [], []
-        
-        start = newInterval[0]
-        end = newInterval[1]
+        #will split intervals into left, insert then right
+        l,r = [], []
+        start, end = newInterval[0], newInterval[1]
         
         for curr in intervals:
-            # interval is to the left of target
+            #newInterval is after curr
             if start > curr[1]:
                 l.append(curr)
                 
-            # interval is to the right of target
+            #newInterval is before curr
             elif end < curr[0]:
                 r.append(curr)
                 
-            # overlap
-            else: 
-                start = min(start, curr[0])
-                end = max(end, curr[1])
-        
+            #newInterval and curr overlap
+            else:
+                start = min(curr[0], start)
+                end = max(curr[1], end)
+                
+        #add all intervals together
         return l + [[start,end]] + r
             
+        
