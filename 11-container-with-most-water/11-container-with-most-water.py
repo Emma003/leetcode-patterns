0cd0b043f607b8517902695833f88476447f317e
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-08-31 10:38:13 -0400

    Time: 1441 ms (15.69%), Space: 26.8 MB (92.01%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,18 +1,23 @@
-
-def container_volume(x1, x2, y1, y2):
-    return (x2-x1) * min(y1,y2)
-
+def getArea(arr, l,r):
+    lVal = arr[l]
+    rVal = arr[r]
+    xDiff = r-l
+    
+    return min(lVal,rVal) * xDiff
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height)-1
-        max_vol = 0
+        left,right = 0, len(height)-1
+        maxArea = 0
         
-        while l<r:
-            max_vol = max(max_vol, container_volume(l+1, r+1, height[l], height[r]))
-            if height[l] < height[r]:
-                l+=1
-            else: 
-                r-=1
-        return max_vol
+        while left<right:
             
+            maxArea = max(maxArea, getArea(height,left,right))
+            
+            if height[left] < height[right]:
+                left += 1
+            else: 
+                right -= 1
+                
+        return maxArea
+                
