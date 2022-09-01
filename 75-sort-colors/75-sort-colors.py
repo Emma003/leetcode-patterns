40dc8ff2793c2fe1dafcad41dbe3aec97afbe3f7
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-01 00:48:22 -0400

    Time: 59 ms (34.07%), Space: 13.8 MB (64.54%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,22 +1,28 @@
+
+def swap(arr, p1, p2):
+    tmp = arr[p1]
+    arr[p1] = arr[p2]
+    arr[p2] = tmp
+
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColors(self, arr: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        low, high, i = 0, len(nums)-1, 0
-
-        # quick sort
-        while i <= high:
-            if nums[i] == 0:
-                nums[i] = nums[low]
-                nums[low] = 0
-                low += 1
+        
+        curr, lo, hi = 0, 0 , len(arr)-1
+        
+        while curr <= hi:
+            if arr[curr] == 0:
+                swap(arr, curr, lo)
+                lo += 1
+                curr += 1
                 
-            # dont increment i if you swap the high value (continue)
-            elif nums[i] == 2:
-                nums[i] = nums[high]
-                nums[high] = 2
-                high -= 1
-                continue
-
-            i += 1
+            elif arr[curr] == 2:
+                swap(arr, curr, hi)
+                hi -= 1
+                
+            else:
+                curr += 1
+            
+            
