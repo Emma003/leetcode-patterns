9d0c15b91c51691d030bdb888985e2eb391936dc
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-02 17:31:14 -0400

    Time: 102 ms (72.88%), Space: 14 MB (31.65%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,29 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        dict = {}
-        for letter in s1:
-            if letter not in dict:
-                dict[letter] = 0
-            dict[letter] += 1
-
-
-        matches, left = 0, 0
-        for right in range(len(s2)):
-            if s2[right] in dict:
-                dict[s2[right]] -= 1
-                if dict[s2[right]] == 0:
-                    matches += 1
-
-            if matches == len(dict):
-                return True
-
-            if right >= len(s1) - 1:
-                if s2[left] in dict:
-                    if dict[s2[left]] == 0:
-                        matches -= 1
-                    dict[s2[left]] += 1
-
-                left += 1
-
-        return False
+        #load map
+        chars = {}
+        for c in s1:
+            if c not in chars:
+                chars[c] = 0
+            chars[c] += 1
+            
+        #check s2 (sliding window)
+        l, matches = 0, 0
         
+        for r in range(len(s2)):
+            if s2[r] in chars:
+                chars[s2[r]] -= 1
+                if chars[s2[r]] == 0:
+                    matches += 1
+                    
+            if matches == len(chars):
+                return True
+            
+            if r >= len(s1)-1:
+                if s2[l] in chars:
+                    if chars[s2[l]] == 0:
+                        matches -= 1
+                    chars[s2[l]] += 1
+                l+= 1
+                
+                
+        return False
+                    
