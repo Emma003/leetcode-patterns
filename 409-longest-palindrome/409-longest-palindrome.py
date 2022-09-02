149249d8ca12f78c5f7530b53f2ff6781f9b66df
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-02 17:03:14 -0400

    Time: 59 ms (31.96%), Space: 13.9 MB (22.37%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,17 +1,14 @@
 class Solution:
     def longestPalindrome(self, str1: str) -> int:
-        map = {}
+        map = set()
         count = 0
         
         for s in str1:
             if s not in map:
-                map[s] = 1
+                map.add(s)
             else:
-                map[s] -= 1
-                if map[s] % 2 == 0:
-                    count += 2
-                    if map[s] == 0:
-                        del map[s]
+                count += 2
+                map.remove(s)
                     
         if len(map) > 0:
             return count + 1
