ef76718bd024e8142a4df1b06c948df57e240a57
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-02 23:09:45 -0400

    Time: 3706 ms (5.45%), Space: 30.1 MB (23.77%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,13 +1,43 @@
+
+def quickSelect(arr, l, r, k):
+    if l > r:
+        return -1
+    
+    targetIndex = len(arr) - k
+    
+    currPartitionIndex = partition(arr, l, r)
+    
+    if currPartitionIndex == targetIndex:
+        return arr[currPartitionIndex]
+    elif currPartitionIndex > targetIndex:
+        return quickSelect(arr, l, currPartitionIndex - 1,k)
+    elif currPartitionIndex < targetIndex:
+        return quickSelect(arr, currPartitionIndex + 1, r,k)
+
+
+def partition (arr, l, r):
+    #pivot == arr[r]
+    
+    #p = tail of less than group
+    p = l
+    for i in range(l,r):
+        #if curr < pivot, shift to less group
+        if arr[i] <= arr[r]:
+            swap(arr, p, i)
+            p += 1
+            
+    #swap tail of less group (p) with pivot
+    swap(arr, p, r)
+
+    return p
+
+def swap(arr, i1, i2):
+    tmp = arr[i1]
+    arr[i1] = arr[i2]
+    arr[i2] = tmp
+
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        maxHeap = []
-        heapq.heapify(maxHeap) #O(n)
-        
-        for num in nums:
-            heapq.heappush(maxHeap, -num) #O(n)
+        return quickSelect(nums, 0, len(nums)-1, k)
         
         
-        for i in range(k-1):
-            heapq.heappop(maxHeap)
-            
-        return -heapq.heappop(maxHeap)
