773b878605f89bcb71c42f7a55af443e49adf344
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-02 23:22:10 -0400

    Time: 3156 ms (9.56%), Space: 27.2 MB (38.61%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,54 +1,32 @@
 
-def quickSelect(arr, l, r, k):
-    if l > r:
-        return -1
-    
-    targetIndex = len(arr) - k
-    
-    currPartitionIndex = partition(arr, l, r)
-    
-    if currPartitionIndex == targetIndex:
-        return arr[currPartitionIndex]
-    elif currPartitionIndex > targetIndex:
-        return quickSelect(arr, l, currPartitionIndex - 1,k)
-    elif currPartitionIndex < targetIndex:
-        return quickSelect(arr, currPartitionIndex + 1, r,k)
+def partition(nums: List[int], left: int, right: int) -> int:
+    pivot, fill = nums[right], left
 
+    for i in range(left, right):
+        if nums[i] <= pivot:
+            nums[fill], nums[i] = nums[i], nums[fill]
+            fill += 1
 
-def partition (arr, l, r):
-    #pivot == arr[r]
-    
-    #p = tail of less than group
-    p = l
-    for i in range(l,r):
-        #if curr < pivot, shift to less group
-        if arr[i] <= arr[r]:
-            arr[p], arr[i] = arr[i], arr[p]
-            p += 1
-            
-    #swap tail of less group (p) with pivot
-    arr[p], arr[r] = arr[r], arr[p]
+    nums[fill], nums[right] = nums[right], nums[fill]
 
-    return p
+    return fill
 
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        targetIndex = len(nums) - k
-        l, r = 0, len(nums)-1
+        k = len(nums) - k
+        left, right = 0, len(nums) - 1
         
-        while l <= r:
-            pivot = partition(nums, l, r)
-
-            if pivot < targetIndex:
-                l = pivot + 1
-            elif pivot > targetIndex:
-                r = pivot - 1
+        while left < right:
+            pivot = partition(nums, left, right)
+            
+            if pivot < k:
+                left = pivot + 1
+            elif pivot > k:
+                right = pivot - 1
             else:
-                return nums[targetIndex]
-      
-        
-        
-        # return quickSelect(nums, 0, len(nums)-1, k)
+                break
+
+        return nums[k]
         
         
