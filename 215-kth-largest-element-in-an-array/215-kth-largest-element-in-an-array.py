b49d0e49be4e0b1f4c8eeb26d51fce34f69df1cf
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-04 13:52:20 -0400

    Time: 3789 ms (5.03%), Space: 30 MB (25.17%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,32 +1,38 @@
 
-def partition(nums: List[int], left: int, right: int) -> int:
-    pivot, fill = nums[right], left
-
-    for i in range(left, right):
-        if nums[i] <= pivot:
-            nums[fill], nums[i] = nums[i], nums[fill]
-            fill += 1
-
-    nums[fill], nums[right] = nums[right], nums[fill]
-
-    return fill
-
+def quickSelect(arr, k, l, r):
+    if l>r:
+        return
+    
+    targetIndex = len(arr) - k
+    
+    pivot = partition(arr, l, r)
+    
+    if pivot == targetIndex:
+        return arr[pivot]
+    elif pivot > targetIndex:
+        return quickSelect(arr, k, l, pivot-1)
+    else:
+        return quickSelect(arr, k, pivot+1, r)
+        
+        
+def partition(arr, l, r):
+    i = l
+    
+    for j in range(l, r):
+        if arr[j] <= arr[r]:
+            arr[i], arr[j] = arr[j], arr[i]
+            i+= 1
+            
+    arr[i], arr[r] = arr[r], arr[i]
+    
+    return i
+    
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        k = len(nums) - k
-        left, right = 0, len(nums) - 1
+        #quick select: target index is len(nums)-k
+        #partition: find a pivot, move less than to left and more than to the right
         
-        while left < right:
-            pivot = partition(nums, left, right)
-            
-            if pivot < k:
-                left = pivot + 1
-            elif pivot > k:
-                right = pivot - 1
-            else:
-                break
-
-        return nums[k]
+        return quickSelect(nums, k, 0, len(nums)-1)
         
         
