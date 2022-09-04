24d4f6250c361fc4f67fb2ff96cb049652039d0b
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-04 17:15:17 -0400

    Time: 210 ms (54.35%), Space: 15.2 MB (81.20%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,39 +1,34 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        result_indexes = []
-
-        # loading all chars in pattern into a dict
-        dict = {}
-        for c in p:
-            if c not in dict:
-              dict[c] = 0
-            dict[c] += 1
-
-
-        window_start, matches = 0, 0
-        # iterating through chars of the string
-        for window_end in range(len(s)):
-            right = s[window_end]
-
-            # decrement char from dict if found in string, increment matches if all instances of that char are found
-            if right in dict:
-                dict[right] -= 1
-                if dict[right] == 0:
-                    matches += 1
-
-            # record start index of perm. window 
-            if matches == len(dict):
-              result_indexes.append(window_start)
-
-            # slide window forward
-            if window_end >= len(p) - 1:
-              left = s[window_start]
-              if left in dict:
-                if dict[left] == 0:
-                  matches -= 1
-                dict[left] += 1
-
-              window_start += 1
-
-        return result_indexes
+        res = []
+        map = {}
         
+        #load p in map
+        for c in p:
+            if c not in map:
+                map[c] = 0
+            map[c] += 1
+            
+        #iterate over p
+        l = 0
+        matches = 0
+        for end in range(len(s)):
+            r = s[end]
+            
+            if r in map:
+                map[r] -= 1
+                if map[r] == 0:
+                    matches += 1
+                    
+            if matches == len(map):
+                res.append(l)
+                
+            if end >= len(p)-1:
+                if s[l] in map:
+                    if map[s[l]] == 0:
+                        matches -= 1
+                    map[s[l]] += 1
+                    
+                l += 1
+                
+        return res
