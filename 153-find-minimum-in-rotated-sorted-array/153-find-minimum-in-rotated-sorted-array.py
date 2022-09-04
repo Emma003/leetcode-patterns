f91d603e4328f2cb68097f13a5d9f22f95550f90
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-04 13:39:34 -0400

    Time: 68 ms (43.20%), Space: 14.4 MB (23.54%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,30 +1,33 @@
-
-
-
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        self.res = math.inf
+        #find whether left or right side is strictly increasing
+            #if left side is strictly incr, check right and vice versa
+        #check if arr is fully sorted (l<r)
+        #also update max with mid
         
+        self.minVal = math.inf
         
         def binSearch(arr, l, r):
-            if l > r:
-                return 
-            
-            #array is already fully sorted (arr[0] is the min)
-            if arr[l] < arr[r]:
-                self.res = min(self.res, arr[l])
+            #if you traversed whole array
+            if l>r: 
                 return
             
-            mid = (l+r) // 2
-            self.res = min(self.res, arr[mid])
-            #check if left is strictly increasing
+            #if array is already fully sorted, no need to keep traversing
+            if arr[l] < arr[r]:
+                self.minVal = min(self.minVal, arr[l])
+                return
+            
+            #find strictly incr half and traverse the other half
+            mid = (l+r)//2
+            self.minVal = min(self.minVal, arr[mid])
+            
+            #left is strictly incr (MID IS GREATER THAN OR EQUAL TO!!)
             if arr[mid] >= arr[l]:
                 binSearch(arr, mid+1, r)
-
+                
             #right is strictly incr
-            else:
+            else: 
                 binSearch(arr, l, mid-1)
-        
-        
+                
         binSearch(nums, 0, len(nums)-1)
-        return self.res
+        return self.minVal
