727d431d0bda6a4ea2f72239434523beba2903a8
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-04 01:03:34 -0400

    Time: 2338 ms (81.14%), Space: 310.5 MB (12.37%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,37 +1,75 @@
+# def buildGraph(edges):
+#     graph = {}
+    
+#     for a, b in edges:
+        
+#         if a not in graph:
+#             graph[a] = []
+#         if b not in graph:
+#             graph[b] = []
+            
+#         graph[a].append(b)
+#         graph[b].append(a)
+        
+#     return graph
+
+
+
+# def hasPath(graph, src, dst, visited):
+#     #found path
+#     if src == dst:
+#         return True
+    
+#     #already visited
+#     if src in visited:
+#         return False
+    
+#     #add to visited
+#     visited[src] = True
+    
+#     #iterate over neighbors
+#     for neighbor in graph[src]:
+#         if hasPath(graph, neighbor, dst, visited):
+#             return True
+        
+#     return False
+
+
+def undirected_path(edges, node_A, node_B):
+    graph = buildGraph(edges)
+    visited = set()
+
+    return dfs(graph, node_A, node_B, visited)
+  
+    
+def dfs(graph, src, dst, visited):
+    if src == dst:
+        return True
+
+    if src in visited:
+        return False
+    visited.add(src)
+
+
+    for nei in graph[src]:
+        if dfs(graph, nei, dst, visited):
+            return True
+    return False
+
+
 def buildGraph(edges):
     graph = {}
-    
+
     for a, b in edges:
-        
         if a not in graph:
             graph[a] = []
         if b not in graph:
             graph[b] = []
-            
+    
         graph[a].append(b)
         graph[b].append(a)
-        
+
     return graph
-
-
-def hasPath(graph, src, dst, visited):
-    #found path
-    if src == dst:
-        return True
-    
-    #already visited
-    if src in visited:
-        return False
-    
-    #add to visited
-    visited[src] = True
-    
-    #iterate over neighbors
-    for neighbor in graph[src]:
-        if hasPath(graph, neighbor, dst, visited):
-            return True
-        
-    return False
 
 
 class Solution:
@@ -40,5 +78,6 @@ class Solution:
         #convert matrix to adjacency list
         graph = buildGraph(edges)
         
-        #find path
-        return hasPath(graph, source, destination, {})
+        # #find path
+        # return hasPath(graph, source, destination, {})
+        return dfs(graph, source, destination, set())
