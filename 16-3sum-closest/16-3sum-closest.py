0b2721dc0feece068c0b1db9bbc54c10305edbed
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-09 18:11:52 -0400

    Time: 7113 ms (54.53%), Space: 14 MB (95.21%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,28 +1,49 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        #  i l   r   sum: 1
-        # -4 1 3 4 7 target:2
-        nums.sort() #time compl O(nlogn)
+        nums.sort()
         min_diff = math.inf
+        
         for i,n in enumerate(nums):
-            l,r = i+1, len(nums)-1
-            while l<r:
-                curr_diff = target - (n + nums[l]+nums[r])
                 
-                # triplet sum = target
+            l,r = i+1, len(nums)-1
+
+            while l < r:
+                sum = n + nums[l] + nums[r]
+                
+                curr_diff = target - sum
+                
                 if curr_diff == 0:
                     return target
                 
                 if abs(curr_diff) < abs(min_diff):
                     min_diff = curr_diff
+                    
                 
                 if curr_diff < 0:
                     r -= 1
-                else: 
+                else:
                     l += 1
                     
         return target - min_diff
                 
-            
-            
         
+#         nums.sort() #time compl O(nlogn)
+#         min_diff = math.inf
+#         for i,n in enumerate(nums):
+#             l,r = i+1, len(nums)-1
+#             while l<r:
+#                 curr_diff = target - (n + nums[l]+nums[r])
+                
+#                 # triplet sum = target
+#                 if curr_diff == 0:
+#                     return target
+                
+#                 if abs(curr_diff) < abs(min_diff):
+#                     min_diff = curr_diff
+                
+#                 if curr_diff < 0:
+#                     r -= 1
+#                 else: 
+#                     l += 1
+                    
+#         return target - min_diff
