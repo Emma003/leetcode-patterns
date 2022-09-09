51139b973910b0fa8cd86c694797f821e185d837
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-09 10:21:10 -0400

    Time: 77 ms (5.44%), Space: 14 MB (21.71%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -6,30 +6,21 @@ class Solution:
         #if it hasnt changed positions: TRUE
         #if it has changed positions AND directions
         
-        x, y, dx, dy = 0, 0, 0, 1
+        dx, dy = 0, 1
+        x, y = 0, 0
+        
+        
         for i in instructions:
-            if i == 'R': dx, dy = dy, -dx
-            if i == 'L': dx, dy = -dy, dx
-            if i == 'G': x, y = x + dx, y + dy
-        return (x, y) == (0, 0) or (dx, dy) != (0,1)
-        
-        # xDir, yDir = 0, 1
-        # xPos, yPos = 0, 0
-        
-        
-#         for instr in instructions:
-#             if instr == 'G':
-#                 xPos += xDir
-#                 yPos += yDir
+            if i == 'G':
+                x += dx
+                y += dy
 
-#             elif instr == 'L':
-#                 xDir = -1*yDir
-#                 yDir = xDir
-#             elif instr == 'R':
-#                 xDir = yDir
-#                 yDir = -1*xDir
+            if i == 'L':
+                dx, dy = -dy, dx
+            if i == 'R':
+                dx, dy = dy, -dx
             
-#         return (xPos, yPos) == (0,0) or (xDir, yDir) != (0,1)
+        return (x, y) == (0,0) or (dx, dy) != (0,1)
     
     
 
