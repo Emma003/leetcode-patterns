bad5a273dc6f819c2afb519840198bba18364931
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-08 20:08:50 -0400

    Time: 79 ms (21.83%), Space: 14.3 MB (23.60%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,33 +1,34 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        #find whether left or right side is strictly increasing
-            #if left side is strictly incr, check right and vice versa
-        #check if arr is fully sorted (l<r)
-        #also update max with mid
         
-        self.minVal = math.inf
+        self.min = math.inf
         
         def binSearch(arr, l, r):
-            #if you traversed whole array
-            if l>r: 
-                return
+            if l > r:
+                return 
             
-            #if array is already fully sorted, no need to keep traversing
+            #check whether the array is already fully sorted
             if arr[l] < arr[r]:
-                self.minVal = min(self.minVal, arr[l])
+                self.min = min(self.min, arr[l])
                 return
-            
-            #find strictly incr half and traverse the other half
-            mid = (l+r)//2
-            self.minVal = min(self.minVal, arr[mid])
-            
-            #left is strictly incr (MID IS GREATER THAN OR EQUAL TO!!)
-            if arr[mid] >= arr[l]:
-                binSearch(arr, mid+1, r)
                 
-            #right is strictly incr
-            else: 
+            mid = (l+r) // 2
+            
+            #check middle is minimum
+            self.min = min(self.min, arr[mid])
+            
+            
+            #check if left is strictly increasing or right is strictly increasing
+            if arr[l] > arr[mid]:
                 binSearch(arr, l, mid-1)
                 
+            else:
+                binSearch(arr, mid+1, r)
+                
+                
+                
         binSearch(nums, 0, len(nums)-1)
-        return self.minVal
+        return self.min
+            
+            
+            
