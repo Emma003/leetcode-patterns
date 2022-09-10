4f202811776e8809750cb11384858dfa2fff3e52
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-09 23:32:47 -0400

    Time: 6401 ms (5.00%), Space: 29 MB (29.00%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,12 +4,12 @@ class Solution:
         map = set(nums)
         maxLength = 0
         
-        for i in range(len(nums)):
+        for n in nums:
             currSeq = 1
             
             #check for beginning of conseq sequence
-            if (nums[i] - 1) not in map:
-                nextNum = nums[i] + 1
+            if (n - 1) not in map:
+                nextNum = n + 1
                 
                 while nextNum in map:
                     currSeq += 1
