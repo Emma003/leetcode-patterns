fceca5a9b3f6535c81883aa68b896a5fece24102
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-12 14:25:42 -0400

    Time: 141 ms (36.87%), Space: 17.5 MB (50.58%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,20 +1,17 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if len(intervals) == 0:
-            return 0
-        
         intervals.sort(key = lambda i:i[0])
-        
-        #the heap will only hold end times
         minRooms = []
         heapq.heapify(minRooms)
+        
+        #push end times
         heapq.heappush(minRooms, intervals[0][1])
         
         for i in range(1, len(intervals)):
             start = intervals[i][0]
             end = intervals[i][1]
             
-            #room is free
+            #if overlap, add to heap
             if start >= minRooms[0]:
                 heapq.heappop(minRooms)
                 
