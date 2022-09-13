543fc7fba18fe88250c75e340c2f761e6d3c7c17
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-12 22:08:32 -0400

    Time: 94 ms (80.85%), Space: 17.5 MB (50.56%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,20 +1,18 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort(key = lambda i:i[0])
+        
         minRooms = []
         heapq.heapify(minRooms)
-        
-        #push end times
         heapq.heappush(minRooms, intervals[0][1])
         
         for i in range(1, len(intervals)):
             start = intervals[i][0]
             end = intervals[i][1]
             
-            #if overlap, add to heap
             if start >= minRooms[0]:
                 heapq.heappop(minRooms)
                 
             heapq.heappush(minRooms, end)
-                
+            
         return len(minRooms)
