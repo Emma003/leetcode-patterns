907052247d70a5bd8e2221dffcea46ea0cb7cd1a
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-19 22:21:02 -0400

    Time: 6351 ms (5.00%), Space: 25.5 MB (97.88%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,38 +1,43 @@
+# [2,3,6,1,5,4]
 
-def quickSelect(arr, k, l, r):
-    if l>r:
-        return
-    
-    targetIndex = len(arr) - k
-    
-    pivot = partition(arr, l, r)
-    
-    if pivot == targetIndex:
-        return arr[pivot]
-    elif pivot > targetIndex:
-        return quickSelect(arr, k, l, pivot-1)
-    else:
-        return quickSelect(arr, k, pivot+1, r)
-        
-        
 def partition(arr, l, r):
-    i = l
+    #r is pivot
+    i = l #tail of small values
     
     for j in range(l, r):
+        
+        #if arr[i] < arr[pivot], swap i and j
         if arr[j] <= arr[r]:
             arr[i], arr[j] = arr[j], arr[i]
-            i+= 1
+            i += 1
             
+    #swap i and pivot
     arr[i], arr[r] = arr[r], arr[i]
     
     return i
-    
+
 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        #quick select: target index is len(nums)-k
-        #partition: find a pivot, move less than to left and more than to the right
+        #the kth largest element in the array when sorted should be at position len(nums)-k
+        target = len(nums) - k
         
-        return quickSelect(nums, k, 0, len(nums)-1)
+        #quick select to find right position
+        l,r = 0, len(nums)-1
         
-        
+        while l<r:
+            #pivot is right most
+            sortedIndex = partition(nums, l, r)
+
+            #if sorted index is at k, return element
+            if sortedIndex < target:
+                l = sortedIndex + 1
+                
+            elif sortedIndex > target:
+                r = sortedIndex - 1
+                
+            else:
+                break
+        return nums[target]
+
+            
