d31c8ba5f98fe259ef77b49c6c8e34c8fc9937c1
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-19 21:49:07 -0400

    Time: 250 ms (13.92%), Space: 19.1 MB (31.67%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -3,23 +3,29 @@ class Solution:
         map = {}
         res = []
         
-        #count all letters per word 
-        for word in strs:
-            count = [0] * 26
+        #keep track of the number of letters in each word
+        
+        
+        
+        #populate the array for each word
+        for string in strs:
             
-            for char in word:
-                charIndex = ord(char) - ord('a')
-                count[charIndex] += 1
+            #initialize count array to 26 elements
+            count = [0] * 26 
+            
+            
+            for char in string:
+                charNum = ord('a') - ord(char)
+                count[charNum] += 1
                 
-            #create map with countArray:list of words
+            #create entry in map
             if tuple(count) not in map:
                 map[tuple(count)] = []
-            map[tuple(count)].append(word)
+            map[tuple(count)].append(string)
             
         
-        #iterate through map and lists of words to result array
-        for anagrams in map.values():
-            res.append(list(anagrams))
-            
+        #iterate over values of map and add to result
+        for group in map.values():
+            res.append(group)
             
         return res
