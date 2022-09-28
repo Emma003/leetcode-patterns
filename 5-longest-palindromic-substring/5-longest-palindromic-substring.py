7f13a8ddbc3d87aef0a70dda3504b0beb2535687
Emma003 <90812669+Emma003@users.noreply.github.com>
2022-09-28 18:15:22 -0400

    Time: 922 ms (65.67%), Space: 13.8 MB (89.73%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,28 +1,25 @@
-
-
-        
-
-
-
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        self.maxLength = 0
+        #for each index, search outwards 
+        #check even and odd
+        self.maxWindowSize = 0
         self.res = ""
         
-        def helper(s,l,r):
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r-l+1) > self.maxLength:
-                    self.maxLength = r-l+1
-                    self.res = s[l:r+1]
-                
+        def outwards(arr, l, r):
+            while l >= 0 and r < len(arr) and arr[l] == arr[r]:
+                if (r-l)+1 > self.maxWindowSize:
+                    self.res = arr[l:r+1]
+                    self.maxWindowSize = (r-l)+1
+                    
                 l -= 1
                 r += 1
                 
+        
         for i in range(len(s)):
             #odd
-            helper(s,i,i)
+            outwards(s, i, i)
             
             #even
-            helper(s,i,i+1)
-            
+            outwards(s, i, i+1)
+                
         return self.res
